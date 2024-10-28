025aa6eca8d1ccec9a7d451744adecca5adc1912
xsjk <xsjk17@outlook.com>
2024-10-28 10:52:32 +0800

    Add CrossfeedDevice

--- pkg/layers/mac_test.go
+++ pkg/layers/mac_test.go
@@ -27,49 +27,57 @@ func TestMACLayer(t *testing.T) {
 
 	var preamble = modem.DigitalChripConfig{N: 4, Amplitude: 0x7fffffff}.New()
 
-	var MACLayer = MACLayer{
-		PhysicalLayer: PhysicalLayer{
-			Device: &LoopbackDevice{
-				SampleRate: 48000,
-			},
-			Decoder: Decoder{
-				Demodulator: modem.Demodulator{
-					Preamble:                 preamble,
-					CarrierSize:              CARRIER_SIZE,
-					CorrectionThreshold:      fixed.FromFloat(CORRECTION_THRESHOLD),
-					DemodulatePowerThreshold: fixed.FromFloat(POWER_THRESHOLD),
-					OutputChan:               make(chan []byte, 10),
+	var layers [2]MACLayer
+	var devices [2]Device
+	var addresses [2]MACAddress = [2]MACAddress{0x0, 0x1}
+
+	devices[0], devices[1] = (&CrossfeedDeviceManager{SampleRate: 48000}).Generate()
+
+	for i := range layers {
+		layers[i] = MACLayer{
+			PhysicalLayer: PhysicalLayer{
+				Device: devices[i],
+				Decoder: Decoder{
+					Demodulator: modem.Demodulator{
+						Preamble:                 preamble,
+						CarrierSize:              CARRIER_SIZE,
+						CorrectionThreshold:      fixed.FromFloat(CORRECTION_THRESHOLD),
+						DemodulatePowerThreshold: fixed.FromFloat(POWER_THRESHOLD),
+						OutputChan:               make(chan []byte, 10),
+					},
+					BufferSize: INPUT_BUFFER_SIZE,
 				},
-				BufferSize: INPUT_BUFFER_SIZE,
-			},
-			Encoder: Encoder{
-				Modulator: modem.Modulator{
-					Preamble:      preamble,
-					CarrierSize:   CARRIER_SIZE,
-					BytePerFrame:  BYTE_PER_FRAME,
-					FrameInterval: FRAME_INTERVAL,
+				Encoder: Encoder{
+					Modulator: modem.Modulator{
+						Preamble:      preamble,
+						CarrierSize:   CARRIER_SIZE,
+						BytePerFrame:  BYTE_PER_FRAME,
+						FrameInterval: FRAME_INTERVAL,
+					},
+					BufferSize: OUTPUT_BUFFER_SIZE,
 				},
-				BufferSize: OUTPUT_BUFFER_SIZE,
 			},
-		},
-		Address:    MACAddress(0x01),
-		ACKTimeout: 100 * time.Millisecond,
-		OutputChan: make(chan []byte),
+			Address:    addresses[i],
+			ACKTimeout: 100 * time.Millisecond,
+			OutputChan: make(chan []byte),
+		}
 	}
 
-	MACLayer.Open()
+	layers[0].Open()
+	layers[1].Open()
 
 	inputBytes := make([]byte, 1000)
 	rand.Read(inputBytes)
 
-	go MACLayer.Send(0x01, inputBytes)
+	go layers[0].Send(addresses[1], inputBytes)
 
-	output := MACLayer.Receive()
+	output := layers[1].Receive()
 
 	t.Logf("len(inputBytes) = %d, len(output) = %d", len(inputBytes), len(output))
 	if !reflect.DeepEqual(inputBytes, output) {
 		t.Errorf("inputBytes and outputBytes are different")
 	}
 
-	MACLayer.Close()
+	layers[0].Close()
+	layers[1].Close()
 }
